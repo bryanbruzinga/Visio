fe382c341a09619147c06b264f2d3b18cd106a85
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-05-14 06:24:52 -0300

    aplicação base concluída

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./app.css";
 
 function App() {
+  const [dados, setDados] = React.useState(null);
+
+  const baseUrl = "https://jsonplaceholder.typicode.com/users";
+
+  async function puxarDados() {
+    const informacoes = await axios.get(baseUrl);
+    setDados(informacoes.data);
+  }
+
+  function deleteUser(index) {
+    const copiaDados = [...dados];
+    copiaDados.splice(index, 1);
+    setDados(copiaDados);
+  }
+
+  function ordenar() {
+    if (dados != null) {
+      dados.sort((a, b) => {
+        return a.name < b.name ? -1 : 1;
+      });
+    }
+  }
+
+  ordenar();
+
+  React.useEffect(() => {
+    puxarDados();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="lista">
+      <h1>Lista de Usuários</h1>
+      {dados &&
+        dados.map((item, index) => (
+          <div className="listaItem" key={item.id}>
+            <div>
+              <span>{item.name}</span>
+              <span>{item.username}</span>
+              <span>{item.email}</span>
+            </div>
+            <button type="button" onClick={() => deleteUser(index)}>
+              Excluir
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
